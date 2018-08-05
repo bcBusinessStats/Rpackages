ede3bc8ae6c21c4ed46f151d826844ad41f24751
Stephanie A. Jernigan <stephanie.jernigan.2@bc.edu>
2018-08-05 09:34:54 -0400

    Add evals data for Labs 8 and 9.

--- R/data.R
+++ R/data.R
@@ -45,7 +45,7 @@
 #' @source \url{https://www.openintro.org/stat/data/mlb11.php, via MLB.com}
 "mlb11"
 
-#' @title Teen Primary Social Network
+#' @title snsdata
 #' @description Excerpt from Pew Research Study on Teens, Social Media, and Privacy from May 21, 2013.  Survey of 12 - 17 year olds; only those teens who used at least one social network site were selected for this excerpt.
 #' @format A data frame with 609 rows and 4 variables:
 #' \describe{
@@ -56,3 +56,33 @@
 #'}
 #' @source \url{http://www.pewinternet.org/dataset/september-2012-teens-and-online-privacy/}
 "SNSdata" 
+
+
+#' @title evals
+#' @description The data are gathered from end of semester student evaluations for a large sample of professors from the University of Texas at Austin. In addition, six students rate the professors' physical appearance. The result is a data frame where each row contains a different course and each column has information on either the course or the professor. For more information, please see Çetinkaya-Rundel M, Morgan KL, Stangl D. 2013 (coming soon). Looking Good on Course Evaluations. CHANCE 26(2).
+#' @format A data frame with 463 rows and 21 variables:
+#' \describe{
+#'   \item{\code{score}}{double Average professor evaluation score}
+#'   \item{\code{rank}}{integer Rank of professor:  teaching, tenure track, tenured.}
+#'   \item{\code{ethnicity}}{integer Ethnicity of professor: not minority, minority}
+#'   \item{\code{gender}}{integer Gender of professor: female, male.}
+#'   \item{\code{language}}{integer Language of school where professor received education: English or non-English.}
+#'   \item{\code{age}}{integer Language of school where professor received education: English or non-English.}
+#'   \item{\code{cls_perc_eval}}{double Percent of students in class who completed evaluation.}
+#'   \item{\code{cls_did_eval}}{integer Number of students in class who completed evaluation.}
+#'   \item{\code{cls_students}}{integer Total number of students in class.}
+#'   \item{\code{cls_level}}{integer Class level: lower, upper.}
+#'   \item{\code{cls_profs}}{integer Number of professors teaching sections in course in sample: single, multiple.}
+#'   \item{\code{cls_credits}}{integer Number of credits of class: one credit (lab, PE, etc.), multi credit.}
+#'   \item{\code{bty_f1lower}}{integer Beauty rating of professor from lower level female: (1) lowest - (10) highest.}
+#'   \item{\code{bty_f1upper}}{integer Beauty rating of professor from upper level female: (1) lowest - (10) highest.}
+#'   \item{\code{bty_f2upper}}{integer Beauty rating of professor from second level female: (1) lowest - (10) highest.}
+#'   \item{\code{bty_m1lower}}{integer Beauty rating of professor from lower level male: (1) lowest - (10) highest.}
+#'   \item{\code{bty_m1upper}}{integer Beauty rating of professor from upper level male: (1) lowest - (10) highest.}
+#'   \item{\code{bty_m2upper}}{integer Beauty rating of professor from second upper level male: (1) lowest - (10) highest.}
+#'   \item{\code{bty_avg}}{double Average beauty rating of professor.}
+#'   \item{\code{pic_outfit}}{integer Outfit of professor in picture: not formal, formal.}
+#'   \item{\code{pic_color}}{integer Color of professor’s picture: color, black & white.} 
+#'}
+#' @source \url{https://www.openintro.org/stat/data/evals.php}
+"evals"
